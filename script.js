f5c1da79d471dc55566554c1ed27c42739feef9b
Mohamed Ahmed <116205645+KiritoBloom@users.noreply.github.com>
2023-10-15 12:51:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const toggle = document.getElementById("toggleDark");
 const body = document.querySelector("body");
 const anchors = document.querySelectorAll("nav a");
 const name = document.getElementById("text-container");
+const about = document.getElementById("about-box");
 
 toggle.addEventListener("click", function () {
-  this.classList.toggle("bi-moon");
-  if (this.classList.toggle("bi-brightness-high-fill")) {
+  if (this.classList.contains("fa-moon")) {
+    this.classList.remove("fa-moon");
+    this.classList.add("fa-sun");
     body.style.background = "white";
     body.style.color = "black";
     name.style.color = "black";
@@ -13,7 +15,11 @@ toggle.addEventListener("click", function () {
       anchor.style.color = "black";
     });
     body.style.transition = "1s ease-out";
+    about.style.background = "linear-gradient(to right, purple, crimson);";
+    about.style.borderColor = "black";
   } else {
+    this.classList.remove("fa-sun");
+    this.classList.add("fa-moon");
     body.style.background = "black";
     body.style.color = "white";
     name.style.color = "white";
@@ -21,5 +27,7 @@ toggle.addEventListener("click", function () {
       anchor.style.color = "white";
     });
     body.style.transition = "1s ease-out";
+    about.style.background = "linear-gradient(to right, purple, crimson);";
+    about.style.borderColor = "white";
   }
 });
