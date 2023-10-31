e1622f1657f101d56fa69115d0780b00c02a59b8
Mohamed Ahmed <116205645+KiritoBloom@users.noreply.github.com>
2023-10-31 22:55:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      entry.target.classList.remove("hidden");
+    } //else {
+    //entry.target.classList.add("hidden");
+    //entry.target.classList.remove("show");
+    // }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
